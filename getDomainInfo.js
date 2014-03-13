f5613f128fb5e3e2d802e14ebe6314e17f268cd8
l3ad89 <mackenzie1989@gmail.com>
2014-03-13 15:10:13 -0400

    V1.6
    Made major changes to the script.  Script will work now for any selected
    data attribute in selectionBox.  Output is then formatted into <div> and
    displayed to user line by line.

--- getDomainInfo.js
+++ getDomainInfo.js
@@ -1,42 +1,61 @@
 /**
  * @author JORGRIFFIN
  */
-$(document).ready(function(){
+
+$(document).ready(function(){	
 	
 	$('#btnSubmit').click(function(){
 		
 		var domainName = document.getElementById('domainName').value;
 		var selectionRequest = "whois";
-		var apikey = "463f58354f20a4cc2f0c836bbd5043f2";
-		var requestedObject;
-				
-		if(document.getElementById('chkExpired').checked){
-			requestedObject = "date_expires";
-		}else if(document.getElementById('chkCreated').checked){
-			requestedObject = "date_created";
-		}else if(document.getElementById('chkRegistered').checked){
-			requestedObject = "registered";
-		}else if(document.getElementById('chkTransfer').checked){
-			requestedObject = "date_transferred";
-		}else if(document.getElementById('chkNameservers').checked){
-			requestedObject = "nameservers";
-		}else if(document.getElementById('chkName').checked){
-			requestedObject = "contacts.name";
-		}
+		var apikey = "463f58354f20a4cc2f0c836bbd5043f2";		
+		var selectionChoice = document.getElementById('selectBox').value;		
 		
 		$.getJSON('http://api.whoapi.com/?domain='+domainName+'&r='+selectionRequest+'&apikey='+apikey,
-			function(retrievedInfo){
-				if(retrievedInfo.status == 0){
-					alert(requestedObject);
-					// show the result
-					console.log(retrievedInfo);
-					alert(requestedObject);
-					$('#requestedInfo').append('<div style="color:white">' + JSON.stringify(retrievedInfo.date_expires)) + "</div>";
-				}else{
+		function(dataObj){
+			if(dataObj.status == 0){
+				//checks for the data that user wants displayed
+				switch(selectionChoice){
+					
+					case "expirationDate":
+						$('#home #requestedInfo').append("<center>Domain Expiration Date: " + JSON.stringify(dataObj.date_expires) + "</center> <br />");
+						break;
+					case "creationDate"	:
+						$('#home #requestedInfo').append("<center>Domain Creation Date: " + JSON.stringify(dataObj.date_created) + "</center> <br />");
+						break;
+					case "registered":
+						$('#home #requestedInfo').append("<center>Domain Registered: " + JSON.stringify(dataObj.registered) + "</center> <br />");
+						break;
+					case "transferDate":
+						$('#home #requestedInfo').append("<center>Domain Transfer Date: " + JSON.stringify(dataObj.date_transferred) + "</center> <br />");
+						break;
+					case "nameserver":
+						$('#home #requestedInfo').append("<center>Domain Nameservers: " + JSON.stringify(dataObj.nameservers) + "</center> <br />");
+						break;
+					case "name":
+						$('#home #requestedInfo').append("<center>Domain Contacts Name: " + JSON.stringify(dataObj.name) + "</center> <br />");
+						break;
+					case "organization":
+						$('#home #requestedInfo').append("<center>Domain Contact Organization: " + JSON.stringify(dataObj.organization) + "</center> <br />");
+						break;
+					case "address":
+						$('#home #requestedInfo').append("<center>Domain Contact Address: " + JSON.stringify(dataObj.full_address) + "</center> <br />");
+						break;
+					case "phone":	
+						$('#home #requestedInfo').append("<center>Domain Contact Phone: " + JSON.stringify(dataObj.phone) + "</center> <br />");
+						break;
+					case "email":
+						$('#home #requestedInfo').append("<center>Domain Contact Email: " + JSON.stringify(dataObj.phone) + "</center> <br />");
+						break;
+				}	
+				
+				
+				
+			}else{
 			// show error
-					$('#requestedInfo').append(retrievedInfo.status_desc);
-				}
-		});
-		
+				console.log(dataObj.status);
+				$('#requestedInfo').html(dataObj.status_desc);
+			}
+		});				
 	});
 });
